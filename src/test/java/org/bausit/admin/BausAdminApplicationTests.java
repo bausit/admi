8f058c3fbf3960210ec0ca4c4fd32f43c5bff4e7
Danny Jiang <danny.jiang@mlb.com>
2020-11-15 17:52:39 -0500

    generated maven project using spring starter

---
+++ src/test/java/org/bausit/admin/BausAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bausit.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BausAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
