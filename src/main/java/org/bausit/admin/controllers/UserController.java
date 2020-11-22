9fed08499d7087c949be93a51d496e0178be8f0e
Danny Jiang <danny.jiang@mlb.com>
2020-11-22 17:24:14 -0500

    using thymeleaf layout

--- src/main/java/org/bausit/admin/controllers/UserController.java
+++ src/main/java/org/bausit/admin/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/users")
 public class UserController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String list() {
         return "admin/users/list";
     }
